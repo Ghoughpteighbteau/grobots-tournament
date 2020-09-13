0145144211984f26117e7755aa1d6ce8b4e42bbe
Alex Bruce <alex@lightshipworks.com>
2020-09-13 16:04:10 -0700

    more work on the rank tourny

--- src/score-tournament.ts
+++ src/score-tournament.ts
@@ -3,12 +3,23 @@ import { Rating } from 'ts-trueskill';
 import * as fs from 'fs';
 import * as crypto from 'crypto';
 
+// This script will run a ratings tournament using trueskill ratings.
+// the advantage of this is that when new sides are introduced, or updated
+// this script will select the highest variance (unknown) sides for matches
+// which means it will figure out as fast as it theoretically can how
+// all the new entrants are, matching them with what it believes are
+// their 15 best matches. This will very quickly figure out its rank.
+
 const DB_FNAME = './sides.json';
 const SIDES_DNAME = './sides';
 function sha(s: string): Hash {
   const hash = crypto.createHash('sha256');
   hash.update(s);
   return hash.digest('hex');
+}
+
+function hashSide(side: string): Hash{
+  return sha(fs.readFileSync(SIDES_DNAME + '/' + side, 'ascii'));
 }
 
 type Hash = string
@@ -51,33 +62,51 @@ try {
   process.exit(1);
 }
 
-let side_files = fs.readdirSync(SIDES_DNAME)
+const sideFiles = fs.readdirSync(SIDES_DNAME)
 
 console.log("Checking for side updates or removals");
-
+const dbRemove = Object.keys(db).filter(side => {
+  let hash: Hash
+  try{
+    hash = hashSide(side);
+  }catch(e){
+    console.log(`failed to load "${side}", removing from list`)
+    return true;
+  }
+  if(db[side].hash !== hash){
+    console.log(`"${side} has been updated! resetting score`);
+    db[side] = { rating: new Rating(), hash };
+  }
+  return false;
+});
+dbRemove.forEach(gone => delete db[gone]);
 
 console.log("Checking for new sides");
-let side: fs.Dirent
-// tslint:disable-next-line:no-conditional-assignment ya ya, I know what I'm doing.
-while ((side = sides.readSync()) !== null) {
-  if (db.has(side.name)) continue
+sideFiles.filter(s=>!(s in db)).forEach(side => {
+  console.log(`New side!: "${side}"`)
   let hash: Hash
   try {
-    hash = sha(fs.readFileSync(SIDES_DNAME + '/' + side.name, 'ascii'));
+    hash = hashSide(side);
   } catch (e) {
-    console.log(`error: while reading file "${side.name}" in an attempt to hash it`, e);
+    console.log(`error: while reading file "${side}" in an attempt to hash it`, e);
     process.exit(1);
   }
 
-  db.push({ rating: new Rating(), name: side.name, hash })
-}
-fs.writeFileSync(DB_FNAME, JSON.stringify(db))
+  db[side] = { rating: new Rating(), hash }
+});
 
 console.log("Writing Database");
-
+fs.writeFileSync(DB_FNAME, JSON.stringify(db, null, '\t'))
 
 console.log("Running Tournament");
 console.log("Picking highest variance side");
-console.log("Finding competitors for a 16 match game");
-console.log("Running tournament!");
-console.log("Updating rankings and writing");
+const [runSide, _ ] = Object.keys(db)
+  .reduce((highestSide,side) => {
+    return highestSide[1] > db[side].rating.pi ?
+      highestSide : [side,db[side].rating.pi]
+}, ['nosides?!.wtf', -999]);
+console.log(`"${runSide}" has the highest variance. Matching`);
+console.log("Finding competitors for a 16 side tourny");
+
+// console.log("Running tournament!");
+// console.log("Updating rankings and writing");
